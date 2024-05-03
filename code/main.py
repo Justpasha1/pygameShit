895ae5c517e4c0fe94f6f175e9da27532e33fde7
raven <serv698bnl@gmail.com>
2024-05-03 23:24:52 +0300

    створив вікно і ОГЦ

--- code/main.py
+++ code/main.py
@@ -1,1 +1,11 @@
 from pygame import *
+init()
+screen = display.set_mode((600, 800))
+def game():
+
+    while True:
+        for i in event.get():
+            if i.type == QUIT:
+                print ('1')
+                return False
+game()
