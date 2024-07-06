fe00e78aca8c7bb6be3a9d9135068af762c8c35a
pashok <justuser166@gmail.com>
2024-07-06 14:14:59 +0300

    some changes

--- code/global_vars.py
+++ code/global_vars.py
@@ -10,10 +10,10 @@ plr = player.Player(5, 2, 1, 0, Image(76*4, 61*4, "images\\portrats\\player.png"
 def test():
     print("Hello world")
 
-button_attack = Buttons(133,150, ["images\\buttons\\rectangle\\base.png","images\\buttons\\rectangle\\hover.png", "images\\buttons\\rectangle\\pressed.png"],99*4,97*4,test)
+button_attack = Buttons(130,150, ["images\\buttons\\rectangle\\base.png","images\\buttons\\rectangle\\hover.png", "images\\buttons\\rectangle\\pressed.png"],398,389,test)
 
-button_heal =Buttons(133,150, ["images\\buttons\\rectangle\\base.png","images\\buttons\\rectangle\\hover.png", "images\\buttons\\rectangle\\pressed.png"],133*4,97*4,test)
-button_deffence = Buttons(133,150, ["images\\buttons\\rectangle\\base.png","images\\buttons\\rectangle\\hover.png", "images\\buttons\\rectangle\\pressed.png"],167*4,97*4,test)
+button_heal =Buttons(130,150, ["images\\buttons\\rectangle\\base.png","images\\buttons\\rectangle\\hover.png", "images\\buttons\\rectangle\\pressed.png"],529,389,test)
+button_deffence = Buttons(130,150, ["images\\buttons\\rectangle\\base.png","images\\buttons\\rectangle\\hover.png", "images\\buttons\\rectangle\\pressed.png"],660,389,test)
 
 heart_icon = Image(16*4, 16*4, "images\\icons\\hp.png", 6*4, 70*4)
 shield_icon = Image(16*4, 16*4, "images\\icons\\shield.png", 36*4, 70*4)
